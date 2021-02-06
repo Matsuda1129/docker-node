dc4072e876086e748eb94083c50bf937062483b1
Matsuda1129 <mk1529ayukat315takoyaki@yahoo.co.jp>
2021-02-06 17:19:39 +0900

    develop6

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
   return res.send('Hello world');
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
